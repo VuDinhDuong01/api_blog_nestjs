1fe4eebd95070ff91881eda368d549151dc3f319
vudinhduong <duong2lophot@gmail.com>
2024-09-08 15:46:52 +0700

    feat: setup user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+const defaultOptions = {
+  type: 'postgres',
+  host:'localhost',
+  port: 5433,
+  username: 'postgres',
+  password: '123456',
+  entities: [User],
+  database: 'blog',
+  synchronize: true,
+};
 @Module({
-  imports: [],
+  imports: [UserModule,
+    TypeOrmModule.forRoot(defaultOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
